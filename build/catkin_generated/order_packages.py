b98f197d69c9b7e20f1c9bd681418aed93abcf78
mobydickhm1851 <liuyc@solab.me.ntu.edu.tw>
2018-11-16 11:00:16 +0800

    rm build devel

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/liuyc/moby_ws/moby_simulator/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/liuyc/moby_ws/moby_simulator/devel;/home/liuyc/moby_ws/obstacle_detector/devel;/home/liuyc/moby_ws/solabot_simulator/devel;/home/liuyc/moby_ws/solabot/devel;/opt/ros/kinetic".split(';') if "/home/liuyc/moby_ws/moby_simulator/devel;/home/liuyc/moby_ws/obstacle_detector/devel;/home/liuyc/moby_ws/solabot_simulator/devel;/home/liuyc/moby_ws/solabot/devel;/opt/ros/kinetic" != "" else []
--- build/od_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liuyc/moby_ws/moby_simulator/src/od_description
-# Build directory: /home/liuyc/moby_ws/moby_simulator/build/od_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
